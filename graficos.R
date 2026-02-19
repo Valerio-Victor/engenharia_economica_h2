6a4f4d3990371e8fa1aac350e2a36384084f0463
Valerio-Victor <victor.dmv@unifei.edu.br>
2026-02-19 11:44:38 -0300

    Finalização da primeira versão da Apostila.

---
+++ graficos.R
@@ -0,0 +1,16 @@
+# PACOTES: ----------------------------------------------------------------
+library(magrittr, include.only = '%>%')
+library(ggplot2)
+library(plotly)
+library(crosstalk)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(sf)
+
+
+# IMPORTAÇÃO DOS DADOS: ---------------------------------------------------
+
+#
+
+# ORGANIZAÇÃO DOS DADOS: --------------------------------------------------
+#
